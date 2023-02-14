7f578f83f2d0d9210dfe2ffbe2b5d557788eac7b
CarlosOrduz777 <carlos.orduz@mail.escuelaing.edu.co>
2023-02-13 22:23:33 -0500

    Get and Post methods added

--- src/main/java/main/services/UserServices.java
+++ src/main/java/main/services/UserServices.java
@@ -1,5 +1,6 @@
 package main.services;
 
+import main.model.User;
 import main.persistence.UserPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,13 @@ import java.util.HashMap;
 @Service
 public class UserServices {
     @Autowired
-    private UserPersistence up = null;
+    private UserPersistence up;
 
-    public HashMap<Integer, String> getUsers(){
-        return up.getuserById();
+    public HashMap<Integer, User> getAllUsers(){
+        return up.getAllUsers();
+    }
+
+    public User createUser(User user){
+        return up.createUser(user);
     }
 }

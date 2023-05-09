6fb26c68cb9c1d69b76e16b99e5494cfd55abdb7
unknown <egonzalezsuarez07@gmail.com>
2023-05-08 20:48:44 -0500

    arreglo cors ip local

--- src/main/java/main/controller/EventController.java
+++ src/main/java/main/controller/EventController.java
@@ -14,7 +14,7 @@ import main.services.EventServices;
 import main.model.Event;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 @RequestMapping("/v1/events")
 public class EventController {
     
